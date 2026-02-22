25bb8a2354aa0d64f5bbe37315cad77e4640c25a
Hiwot Demissie <demissiehiwot32@gmail.com>
2026-02-23 01:11:04 +0400

    second commit

--- Wedding/Wedding/urls.py
+++ Wedding/Wedding/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ForeverLove.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page, name='home')
 ]
